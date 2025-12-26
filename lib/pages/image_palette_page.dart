6b479b21303eb7e238498fc83a1f41fd099cd422
William Shakour (billy1380) <william.shakour@gmail.com>
2025-12-26 00:49:36 +0000

    Implement ImagePalettePage using shared base
    - Converted from placeholder to functional page
    
    - Uses NamedColorPalette like HtmlColourPage
    
    - All 5 palette pages now refactored

--- lib/pages/image_palette_page.dart
+++ lib/pages/image_palette_page.dart
@@ -1,8 +1,13 @@
+import "dart:typed_data";
+
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
-import "package:willshex_triangles/parts/app_drawer.dart";
+import "package:http/http.dart" as http;
+import "package:image/image.dart" as img;
+import "package:willshex_triangles/parts/triangle_generator_page.dart";
+import "package:willshex_triangles/triangles/graphics/named_color_palette.dart";
 
-class ImagePalettePage extends StatefulWidget {
+class ImagePalettePage extends StatelessWidget {
   static const String routePath = "/imagepalette";
 
   static Widget builder(BuildContext context, GoRouterState state) {
@@ -12,60 +17,10 @@ class ImagePalettePage extends StatefulWidget {
   const ImagePalettePage._();
 
   @override
-  State<ImagePalettePage> createState() => _ImagePalettePageState();
-}
-
-class _ImagePalettePageState extends State<ImagePalettePage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const AppDrawer(),
-      appBar: AppBar(
-        title: const Text("From Image"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text("TTB Hexagons over image",
-                style: Theme.of(context).textTheme.titleMedium),
-          ),
-          // Toolbar Placeholder
-          Container(
-            height: 50,
-            color: Colors.grey[200],
-            child: const Center(child: Text("Image Toolbar Placeholder")),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      color: Colors.white,
-                    ),
-                    child: const Center(child: Text("Canvas Placeholder")),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      color: Colors.grey[100],
-                    ),
-                    child: const Center(
-                        child: Text("Image Placeholder\n(Upload/Drop)")),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return TriangleGeneratorPage(
+      title: "Triangles Over Image",
+      paletteProvider: () async => NamedColorPalette(),
     );
   }
 }
